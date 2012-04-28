425a49ae7fd7c87e017b03bf34ada49dfa5e54b7
Justin Soliz <justinsoliz@gmail.com>
2012-04-28 14:04:05 -0700

    adding twitter boostrap

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,5 +15,7 @@ class User < ActiveRecord::Base
   validates :email, :presence => true
   validates :password, :presence => true
 
+  validates_format_of :email, :with => /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i
+
 
 end
